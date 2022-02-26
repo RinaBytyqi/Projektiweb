15bab2ac0aca28ac133f8b66bb741e2c0febfe79
PoemaPolloshka <93519598+PoemaPolloshka@users.noreply.github.com>
2022-02-26 19:19:23 +0100

    Add files via upload
    page where we view results

---
+++ searchResults.php
@@ -0,0 +1,16 @@
+
+        <link rel="stylesheet" href="css/shopstyle.css" >
+        <link rel="stylesheet" href="css/projectstyle.css" >
+        <link rel="stylesheet" href="css/insertstyle.css" >
+
+
+    
+<?php
+include 'header.php';
+
+?>
+<?php
+include_once 'controllers/search.php';
+
+
+?>
